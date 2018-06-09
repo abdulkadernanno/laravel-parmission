8fa0e108ee8250a0a98a24d5dbbbf741343048ee
pop sani <popsani300@gmail.com>
2018-06-09 13:43:56 +0600

    change about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pop sani laravel-parmission</title>
+</head>
+<body>
+     <div class="contanier">
+     	<div class="row">
+     		<header>Laravel Parmission</header>
+     	</div>
+     </div>
+</body>
+</html>
--- resources/views/ffff.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aknanno
- * Date: 6/8/2018
- * Time: 9:16 PM
- */
