bbf030b228905eb8396641c8b6882929b7fbc576
pop sani <popsani300@gmail.com>
2018-06-10 00:13:42 +0600

    dashboard change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
-        </div>
-    </div>
+    
 @endsection
