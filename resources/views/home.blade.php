427335308baefa5974b151612b6d57138c65e690
pop sani <popsani300@gmail.com>
2018-06-10 12:29:36 +0600

    home page panel create

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="wrapper">
+    	<div class="container">
+    		<div class="row">
+    			<div class="panel">
+    				<div class="panel-header">
+    					<h5>laravel permission chack</h5>
+    				</div>
+    				<div class="panel-body">
+    					<div class="col-md-6"></div>
+    				</div>
+    			</div>	
+    		</div>
+    	</div>
+    </div>
+
 
 @endsection
